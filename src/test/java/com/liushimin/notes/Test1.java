6d793a2bdd4bf951f5a6ce0d5f3c1a7aad5cc57d
liushimin <liushiminwust@163.com>
2021-06-25 15:22:23 +0800

    test

---
+++ src/test/java/com/liushimin/notes/Test1.java
@@ -0,0 +1,16 @@
+package com.liushimin.notes;
+
+import org.junit.Test;
+
+/**
+ * @author: liushimin
+ * @date: 2021/6/25 15:19
+ * @description:
+ */
+public class Test1 {
+
+    @Test
+    public void test1() {
+        System.out.println("haha");
+    }
+}
